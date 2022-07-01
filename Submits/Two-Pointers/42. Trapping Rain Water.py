50b7814a807a06cc68da5ec51ab6a6f4d537731f
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-07-01 11:44:09 -0700

    Create 42. Trapping Rain Water.py

---
+++ Submits/Two-Pointers/42. Trapping Rain Water.py
@@ -0,0 +1,23 @@
+class Solution:
+    def trap(self, height) -> int:
+        left, right = 0, len(height)-1
+        maxLeft, maxRight = height[left], height[right] #maxLeft and maxRight will keep track of maxLeft and maxRight
+        result = 0
+        
+        while left < right:
+            if maxLeft < maxRight: # We're moving the max which is minumum between left and right because the amount of water trapped depends on the min height between maxLeft and maxRight
+                left += 1
+                maxLeft = max(maxLeft, height[left])
+                result += (maxLeft - height[left]) # we are supposed to add only positive values to the results as it is not possible to trap negative rain water,
+                # however the value won't be negative because we are updating the maxLeft first
+            else:
+                right -= 1
+                maxRight = max(maxRight, height[right])
+                result += (maxRight - height[right])
+                
+        return result
+    
+'''
+Time Complexity - O(n)
+Space Complexity - O(1)
+'''
